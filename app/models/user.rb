ecaa33767b87c11e8ee7ca5f55f4352bb0aede58
fugyomaru <fugyomaru@gmail.com>
2024-08-03 15:11:21 +0900

    バリデーション設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name_kanji, presence: true,
+                                format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name is invalid. Input full-width characters' }
+  validates :first_name_kanji, presence: true,
+                               format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name is invalid. Input full-width characters' }
+  validates :family_name_kana, presence: true,
+                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana is invalid. Input full-width katakana characters' }
   validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers'
 end
