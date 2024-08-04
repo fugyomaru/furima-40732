5c961dcabc370007a7dca0da088ae39646645ac6
fugyomaru <fugyomaru@gmail.com>
2024-08-04 15:26:34 +0900

    修正 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :family_name_kanji, presence: true,
-                                format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name is invalid. Input full-width characters' }
+                                format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' }
   validates :first_name_kanji, presence: true,
-                               format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name is invalid. Input full-width characters' }
   validates :family_name_kana, presence: true,
                                format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana is invalid. Input full-width katakana characters' }
   validates :first_name_kana, presence: true,
