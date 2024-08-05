c0da3773f6e65ff84dc323d323aeb6efbcbc9650
fugyomaru <fugyomaru@gmail.com>
2024-08-05 11:19:01 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :area
   validates :item_name, presence: true
   validates :item_text, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :charge_id, presence: true
-  validates :area_id, presence: true
-  validates :day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
 
   belongs_to :user
