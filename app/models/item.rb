129cd907d4aef982302eacab768af52e3fac3c0f
fugyomaru <fugyomaru@gmail.com>
2024-08-05 09:08:41 +0900

    モデル作成2 + 出品画面移行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :charge_id, presence: true
+  validates :area_id, presence: true
+  validates :day_id, presence: true
+  validates :price, presence: true
+
   belongs_to :user
 end
