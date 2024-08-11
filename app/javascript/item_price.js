603203014695133027a1bd10596fd2884cbd07bf
fugyomaru <fugyomaru@gmail.com>
2024-08-11 14:35:17 +0900

    javascript 計算結果標示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1)
+    const profit = document.getElementById("profit");
+    profit.innerHTML = priceInput.value - Math.floor(priceInput.value * 0.1)
+  })
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
